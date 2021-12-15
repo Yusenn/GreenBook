31c72520ecdcc0a158026342dbcb192973004475
2003565E@Student.tp.edu.sg <2003565E@Student.tp.edu.sg>
2021-12-15 13:51:15 +0800

    Commit Test

---
+++ Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GreenBook.Server.Data;
+using GreenBook.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GreenBook.Server.Areas.Identity.IdentityHostingStartup))]
+namespace GreenBook.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
